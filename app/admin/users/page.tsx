d3f988d3e0968b2818e90f61e38d85bbb9d117b0
Su-Marié Hörstmann <sumariehorstmann@gmail.com>
2026-06-22 14:10:15 +0200

    Add temporary admin users page

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminUsersPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Admin Users</h1>
+      <p>Admin users dashboard coming soon.</p>
+    </main>
+  );
+}
